e368fcd823885b75b7088b8238b1e686d5897373
Bern <bernnystrom@gmail.com>
2020-12-03 06:01:02 +0800

    Made a list of sample items object with images for users to load in pre-made data

---
+++ src/sample-items.js
@@ -0,0 +1,85 @@
+// This is just some sample data so you don't have to come up with your own ones!
+const items = {
+    item1: {
+        name: "Nutmeg Cappucino",
+        image: "/public/images/nutmeg-coffee.jpg",
+        desc:
+            "Our best-selling espresso infused with Nutmeg spices imported from Morrocco. Perfect for any time of the day.",
+        price: 535,
+        status: "available"
+    },
+  
+    item2: {
+        name: "Pumpkin Spice Latte",
+        image: "public/images/pumpkin-spice-coffee.jpg",
+        desc:
+            "Espresso and steamed milk, combined with a blend of pumpkin and traditional fall spice flavors. Enjoy this seasonal special finished with whipped cream.",
+        price: 429,
+        status: "available"
+    },
+  
+    item3: {
+        name: "Cinnamon Cocoa Latte",
+        image: "public/images/cinnamon-cocoa-coffee.jpg",
+        desc:
+            "The aroma of cinnamon and cocoa makes this mocha coffee hard to resist.",
+        price: 399,
+        status: "unavailable"
+    },
+  
+    item4: {
+        name: "Smoked Butterscotch Latte",
+        image: "public/images/butterscotch-coffee.jpg",
+        desc:
+            "Notes of smoked butterscotch intermingle with our signature espresso, giving your latte a sophisticated new twist.",
+        price: 376,
+        status: "available"
+    },
+  
+    item5: {
+        name: "Dark Cocoa Mocha",
+        image: "public/images/dark-cocoa-coffee.jpg",
+        desc:
+            "Our signature espresso-blend coffee infused with fair trade dark chocolate, topped with whipped cream.",
+        price: 389,
+        status: "available"
+    },
+  
+    item6: {
+        name: "Mocha Cupcake",
+        image: "public/images/mocha-espresso-cupcakes.jpg",
+        desc:
+            "These cupcakes are coffee-infused with a subtle chocolate flavor, and topped with an espresso-spiked buttercream frosting.",
+        price: 315,
+        status: "unavailable"
+    },
+  
+    item7: {
+        name: "Brownie",
+        image: "public/images/brownie.jpg",
+        desc: 
+            "Our fudgy brownies with nuts (or nut-free) and choco chips. Comes with a free icing of your choice 😋 ",
+        price: 246,
+        status: "available"
+    },
+  
+    item8: {
+        name: "Kanelbolle",
+        image: "public/images/cinnamon-roll.jpg",
+        desc: 
+            "A sweet roll from our lovely Scandinavian bakers. Perfect for those 'hygge' and 'fika' moments! 😌 ",
+        price: 269,
+        status: "available"
+    },
+  
+    item9: {
+        name: "Pumpkin Pie",
+        image: "public/images/pumpkin-pie.jpg",
+        desc:
+            "These seasonal pumpkin pies are flavored with cinnamon, powdered ginger, nutmeg, cloves and vanilla.",
+        price: 217,
+        status: "available"
+    }
+};
+  
+export default items;
