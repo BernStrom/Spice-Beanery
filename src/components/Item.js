9f471c0c872ea89f398f7f10a8f11b49ffb8b31a
Bern <bernnystrom@gmail.com>
2020-12-03 09:11:11 +0800

    Set conditions to the button display according to item availability

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,6 +4,7 @@ import { formatPrice } from '../helpers';
 export default class Item extends Component {
     render() {
         const { image, name, price, desc, status } = this.props.details;
+        const isAvailable = status === 'available';
 
         return (
             <li className="menu-item">
@@ -13,7 +14,9 @@ export default class Item extends Component {
                     <span className="price">{formatPrice(price)}</span>
                 </h3>
                 <p>{desc}</p>
-                <button>Add To Order</button>
+                <button disabled={!isAvailable}>
+                    {isAvailable ? 'Add To Order' : 'Sold Out!'}
+                </button>
             </li>
         );
     }
