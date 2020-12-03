85c267e5ad1b8da6399174cde2106c890bdfb5f7
Bern <bernnystrom@gmail.com>
2020-12-04 02:22:20 +0800

    Done app landing page with unique store id generator

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,29 @@
-import { Component } from 'react';
+import { Component, createRef } from 'react';
+import { getFunName } from '../helpers';
 
-export default class StorePicker extends Component {   
+export default class StorePicker extends Component {
+    storeIdInput = createRef();
+
+    goToStore = e => {
+        // 1. Stops the form from submitting
+        e.preventDefault();
+        // 2. Get the text from the input
+        const storeNameId = this.storeIdInput.current.value;
+        // 3. Change the page URL to /store/[whatever-users-entered]
+        this.props.history.push(`/store/${storeNameId}`);
+    }
+    
     render() {
         return (
-            <form className="store-selector">
-                <h2>Please Enter A Store</h2>
-                <input type="text" required placeholder="Store Name"/>
+            <form className="store-selector" onSubmit={this.goToStore}>
+                <h2>Enter A Store Name ID</h2>
+                <input 
+                    type="text"
+                    ref={this.storeIdInput}
+                    required 
+                    placeholder="ID Name" 
+                    defaultValue={getFunName()} 
+                />
                 <button type="submit">Visit Store &#10140;</button>
             </form> 
         );
