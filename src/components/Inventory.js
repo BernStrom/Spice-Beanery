7a34d9574a11ef0fdcb87fbbd0a6bd8da4532e8a
Bern <bernnystrom@gmail.com>
2020-12-04 03:55:00 +0800

    Add an edit item form component with props

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,13 +1,24 @@
 import { Component } from 'react';
 import AddItemForm from './AddItemForm';
+import EditItemForm from './EditItemForm';
 
 export default class Inventory extends Component {
     render() {
         return (
             <div className="inventory">
                 <h2>Inventory</h2>
+                {Object.keys(this.props.items).map(key => (
+                    <EditItemForm 
+                        key={key} 
+                        index={key} 
+                        item={this.props.items[key]} 
+                        updateItem={this.props.updateItem} 
+                    />
+                ))}
                 <AddItemForm addItem={this.props.addItem} />
-                <button onClick={this.props.loadSampleItems}>Load Sample Items</button>
+                <button onClick={this.props.loadSampleItems}>
+                    Load Sample Items
+                </button>
             </div>
         );
     }
