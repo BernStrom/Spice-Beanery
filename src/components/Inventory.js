bdb5d172b37b65c38c2ea8bf7459f2eee8610c14
Bern <bernnystrom@gmail.com>
2020-12-05 13:55:39 +0800

    Done login authentication for inventory section

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -14,17 +14,35 @@ export default class Inventory extends Component {
         loadSampleItems: PropTypes.func
     };
 
+    state = {
+        uid: null,
+        owner: null
+    }
+
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                this.authHandler({ user });
+            }
+        })
+    }
+
     authHandler = async authData => {
         // 1. Look up the current store in the firebase database
         const store = await base.fetch(this.props.storeId, { context: this });
+        console.log(store);
         // 2. Claim it if there is no owner
         if (!store.owner) {
             // Save it as our own
             await base.post(`${this.props.storeId}/owner`, {
                 data: authData.user.uid
-            })
+            });
         }
-        console.log(store);
+        // 3. Set the state of the inventory component to reflect the current user
+        this.setState({
+            uid: authData.user.uid,
+            owner: store.owner || authData.user.uid
+        });
     }
 
     authenticate = provider => {
@@ -35,11 +53,34 @@ export default class Inventory extends Component {
             .then(this.authHandler);
     }
 
+    logout = async () => {
+        console.log('Logging Out!');
+        await firebase.auth().signOut();
+        this.setState({ uid: null });
+    }
+
     render() {
-        // return <Login authenticate={this.authenticate} />;
+        const logout = <button onClick={this.logout}>Log Out!</button>
+
+        // Check if they are logged in
+        if (!this.state.uid) {
+            return <Login authenticate={this.authenticate} />;
+        }
+
+        // Check if they are not the owner of the store
+        if (this.state.uid !== this.state.owner) {
+            return (
+                <div>
+                    <p>Sorry you do not have admin access!</p>
+                    {logout}
+                </div>
+            );
+        }
+
         return (
             <div className="inventory">
                 <h2>Inventory</h2>
+                {logout}
                 {Object.keys(this.props.items).map(key => (
                     <EditItemForm 
                         key={key} 
